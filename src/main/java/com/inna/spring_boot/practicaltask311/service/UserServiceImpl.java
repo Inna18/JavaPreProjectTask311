86d843f2e1aa6ecf18a5821a9f60e662c03c9e42
Inna <kinvdev21@gmail.com>
2022-05-15 16:44:17 +0900

    Third commit: Service layer correction

--- src/main/java/com/inna/spring_boot/practicaltask311/service/UserServiceImpl.java
+++ src/main/java/com/inna/spring_boot/practicaltask311/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.inna.spring_boot.practicaltask311.dao.UserDao;
 import com.inna.spring_boot.practicaltask311.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,28 +18,33 @@ public class UserServiceImpl implements UserService{
         this.userDao = userDao;
     }
 
-
+    @Override
     public User findById(Long id) {
         return userDao.getOne(id);
     }
 
+    @Override
     public List<User> findAll() {
         return userDao.findAll();
     }
 
+    @Override
     public void saveUser(User user) {
         userDao.save(user);
     }
 
+    @Override
     public void deleteById(Long id) {
         userDao.deleteById(id);
     }
 
     @Override
+    @Transactional
     public User getActiveUserByEmail(String email) {
         return userDao.getActiveUserByEmail(email);
     }
 
+    @Override
     public boolean isEmailAlreadyInUse(String email) {
         boolean emailInDB = true;
 
